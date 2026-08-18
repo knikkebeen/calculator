abac9451d0141cbb53ad8779d0c6cca736009216
knikkebeen <gkgithub@outlook.com>
2026-08-18 12:41:20 +0200

    Add functionality to all buttons, can make a basic operation now.

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ function divide(a, b) {
 };
 
 function operate(operator, numOne, numTwo) {
+    numOne = +numOne;
+    numTwo = +numTwo;
     if (operator == '+') {
         return add(numOne, numTwo);
     } else if (operator == '-') {
@@ -28,9 +30,40 @@ function operate(operator, numOne, numTwo) {
     };
 };
 
-let numOne;
-let operator;
-let numTwo;
+let numOne = '';
+let operator = '';
+let numTwo = '';
 
+const container = document.querySelector("#container");
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((bttn) => {
+    bttn.addEventListener("click", (e) => {
+        if (bttn.className == 'clear') {
+            numOne = '';
+            operator = '';
+            numTwo = '';
+            display.textContent = '[cleared, please enter a new operation]';
+        };
+
+        if (bttn.className == 'enter') {
+            display.textContent = operate(operator, numOne, numTwo);
+        };
+
+        if (bttn.className == 'num' && operator == '') {
+            numOne += e.target.textContent;
+            display.textContent = numOne;
+        } else if (bttn.className == 'opp' && operator == '') {
+            operator += e.target.textContent;
+            display.textContent = operator;
+            passover = true;
+        } else if (bttn.className == 'num' && operator != true) {
+            numTwo += e.target.textContent;
+            display.textContent = numTwo;
+        };
+
+    });
+});
 
 
